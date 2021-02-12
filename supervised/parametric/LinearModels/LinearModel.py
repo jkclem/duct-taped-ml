03b2cc1a6c712f3733cfaf62c323e6b011353f27
John Clements <jkclements2016@gmail.com>
2021-02-11 19:12:57 -0500

    Subject: Added documentation to the LinearModel Class | Description: Added documentation to the LinearModel class which will serve as the parent class for other linear models, such as OLS, logistic, LASSO, etc.

--- supervised/parametric/LinearModels/LinearModel.py
+++ supervised/parametric/LinearModels/LinearModel.py
@@ -8,11 +8,31 @@ import numpy as np
 
 class LinearModel():
     """"""
-    def __init__(self, add_coefficient):
+    def __init__(self, add_coefficient=True):
         self.add_coefficient = add_coefficient
         self.beta_hat = None
     
     def predict(self, X):
+        """
+        This function predicts the response values of the input array, X, in 
+        the scale the model is estimated in; e.g. a logistic model will return
+        predictions in log-odds. The columns of X must match the number of 
+        columns on the array on which the model was fit. The ordering must be
+        identical as well for the predictions to mean anything.
+
+        Parameters
+        ----------
+        X : numpy ndarray
+            A n x m matrix, where the n rows represent observations and the m
+            columns represent features of the observations.
+
+        Returns
+        -------
+        numpy ndarray
+            Returns a numpy ndarray with n elements that are the predicted 
+            values of the response for each observation in X.
+
+        """
         
         # If this object needs to add an intercept to new data, add one.
         if self.add_coefficient == True:
