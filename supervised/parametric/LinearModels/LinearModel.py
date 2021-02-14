Subject: Added to fit method for OLS Regression | Description: Added calculation of the corrected TSS to the fit method for OLS regression.

supervised/parametric/LinearModels/LinearModel.py
@@ -129,7 +129,9 @@ class OLS(LinearRegression):
             self._fit_pinv(X, y)
         else:
             self._fit_svd(X, y)
-            
+        
+        self._TSS = np.mean(y - np.mean(y))**2
+        
         return
         
         